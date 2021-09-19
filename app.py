25ebb769f51c97dad9d310d003f901f04f6d33b1
JoseCastanoCoding <j.albertocast@gmail.com>
2021-09-19 18:07:36 +0000

    added login functionality and flash messages in base template

--- app.py
+++ app.py
@@ -63,6 +63,23 @@ def signup():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome Back {}".format(request.form.get("username")))
+            else:
+                flash("Oops! Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Oops! Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 if __name__ == "__main__":
